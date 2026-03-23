83ef603178669a03738a0c4f6284be36c820a30d
ion098 <146852218+ion098@users.noreply.github.com>
2026-03-23 20:22:24 +0000

    Move service_worker to root directory

--- script/main.js
+++ script/main.js
@@ -234,5 +234,5 @@ const main = () => {
     setInterval(find_highlight, 1000 * 60);
 };
 
-navigator.serviceWorker.register("/script/service_worker.js", { scope: "/" });
+navigator.serviceWorker.register("/service_worker.js", { scope: "/" });
 main();
